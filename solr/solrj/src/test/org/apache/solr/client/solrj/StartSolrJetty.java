7afbde2d88832dbc87edb45caa62dd441435a2f3
Nasller <nasller@qq.com>
2021-04-07 17:21:34 +0800

    collection

--- solr/solrj/src/test/org/apache/solr/client/solrj/StartSolrJetty.java
+++ solr/solrj/src/test/org/apache/solr/client/solrj/StartSolrJetty.java
@@ -29,8 +29,8 @@ public class StartSolrJetty
 {
   public static void main( String[] args ) 
   {
-    System.setProperty("solr.solr.home", "F:/StudyProject/lucene-solr/solr/server/solr");
-    System.setProperty("solr.log.dir", "F:/StudyProject/lucene-solr/solr/server/logs/solr.log");
+//    System.setProperty("solr.solr.home", "F:/StudyProject/lucene-solr/solr/server/solr");
+//    System.setProperty("solr.log.dir", "F:/StudyProject/lucene-solr/solr/server/logs/solr.log");
 //    System.setProperty("coreRootDirectory", "shared");
 //    System.setProperty("solr.tests.numeric.dv", "true");
 //    System.setProperty("solr.tests.IntegerFieldType", "solr.IntPointField");
