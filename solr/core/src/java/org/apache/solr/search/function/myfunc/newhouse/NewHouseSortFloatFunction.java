f51d21344967ea09833fa76e89646081f5e9dd51
Nasller <nasller@qq.com>
2022-05-17 11:36:47 +0800

    find

--- solr/core/src/java/org/apache/solr/search/function/myfunc/newhouse/NewHouseSortFloatFunction.java
+++ solr/core/src/java/org/apache/solr/search/function/myfunc/newhouse/NewHouseSortFloatFunction.java
@@ -7,11 +7,14 @@ import org.apache.solr.search.function.myfunc.common.SortUtil;
 
 import java.util.Arrays;
 import java.util.List;
+import java.util.concurrent.ThreadLocalRandom;
 import java.util.stream.Collectors;
 
 public class NewHouseSortFloatFunction extends OnlineMultiFloatFunction {
+    private final ThreadLocalRandom random;
     public NewHouseSortFloatFunction(ValueSource[] sources) {
         super(sources);
+        random = ThreadLocalRandom.current();
     }
 
     @Override
@@ -20,16 +23,22 @@ public class NewHouseSortFloatFunction extends OnlineMultiFloatFunction {
             long id = vals[0].longVal(doc);
             int ranking = vals[1].intVal(doc);
             String city = vals[2].strVal(doc);
-            String requestCity = vals[4].strVal(doc);
-            int cityCount = SortUtil.isNotBlank(requestCity) && requestCity.equals(city) ? 500 : 0;
             String ids = vals[3].strVal(doc);
+            String requestCity = vals[4].strVal(doc);
+            boolean isOriginCity = SortUtil.isNotBlank(requestCity) && city.equals(requestCity);
+            int kfangCount = vals[5].intVal(doc);
+            int spiderCount = vals[6].intVal(doc);
+            int gap = 1000;
             if(SortUtil.isNotBlank(ids)){
                 List<Long> newhouseIds = Arrays.stream(ids.split(",")).map(Long::parseLong).collect(Collectors.toList());
                 if (newhouseIds.contains(id)) {
                     return (200-newhouseIds.indexOf(id))*100;
                 }
             }
-            return ranking + cityCount;
+            if(isOriginCity) return ranking + gap;
+            else if(ranking >= kfangCount) return random.nextInt(kfangCount,kfangCount + gap);
+            else if(ranking >= spiderCount) return random.nextInt(spiderCount,spiderCount + gap);
+            else return random.nextInt(0,gap);
         } catch (Exception e) {
             e.printStackTrace();
         }
@@ -40,5 +49,4 @@ public class NewHouseSortFloatFunction extends OnlineMultiFloatFunction {
     protected String name() {
         return "newHouseSort";
     }
-
 }
